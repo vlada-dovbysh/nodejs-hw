8a596b964630d92d3d4aa9139a0a550c9846c434
vlada-dovbysh <mtcdogzoomtc@gmail.com>
2026-09-22 23:35:35 +0300

    configure auth and notes routes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
@@ -17,8 +20,10 @@ const PORT = process.env.PORT || 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 // Routes
+app.use('/auth', authRoutes);
 app.use(notesRoutes);
 
 // 404 middleware
